852a3d280e9590eb6391ec85a3976675bee710ea
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-04-05 18:01:09 +0300

    master: feat: set up router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import './index.css';
-import { App } from './App.tsx';
-import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import { TodoApp } from './components/Project1/index.tsx';
+
+import './index.css';
+import { Root } from './routes/root.tsx';
+import { ErrorPage } from './routes/error.tsx';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab='home' />);
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root tab='home' />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: '/project',
+		element: <TodoApp />,
+		errorElement: <ErrorPage />,
+	},
+]);
+root.render(<RouterProvider router={router} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
