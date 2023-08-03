d1df3674d101c25112acafd9d8b622f142dbfc84
makiyo <makiyo.ronkko@haltian.com>
2023-08-03 17:32:40 +0300

    master: feat: create a new RandomImage component to fetch random pictures

---
+++ src/components/Version2/RandomImage.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Styles from './Desgin.module.css';
+import { Loader } from '../Loader/Loader';
+
+export const RandomImage = () => {
+	const [imageUrl, setImageUrl] = useState<undefined | string>('');
+
+	useEffect(() => {
+		const fetchRandomImage = async () => {
+			try {
+				const response = await axios.get(
+					'https://picsum.photos/200/300?grayscale',
+					{
+						responseType: 'blob',
+					}
+				);
+				setImageUrl(URL.createObjectURL(response.data));
+			} catch (error) {
+				console.error('Error fetching random image:', error);
+			}
+		};
+
+		fetchRandomImage();
+
+		const intervalId = setInterval(() => {
+			fetchRandomImage();
+		}, 5000);
+
+		return () => {
+			clearInterval(intervalId);
+		};
+	}, []);
+
+	return (
+		<div>
+			{imageUrl ? (
+				<img src={imageUrl} alt='random-img-url' />
+			) : (
+				<div className={Styles.loaderWrapper}>
+					<Loader />{' '}
+				</div>
+			)}
+		</div>
+	);
+};
