0ae03e32ff7e8521d11ace053dffbf27404864ad
makiyo <makiyo.ronkko@haltian.com>
2023-08-03 17:32:13 +0300

    master: feat: create a new Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Styles from './Loader.module.css';
+
+type Width =
+	| 'auto'
+	| 'root'
+	| 'xxxxs'
+	| 'xxxs'
+	| 'xxs'
+	| 'xs'
+	| 'sm'
+	| 'md'
+	| 'lg'
+	| 'xl';
+
+interface LoaderProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
+	className?: string;
+	style?: React.CSSProperties;
+	width?: Width;
+}
+
+export const Loader = ({
+	className = '',
+	width = 'xxs',
+	style,
+	...props
+}: LoaderProps) => {
+	const getWidth = () => {
+		const style: React.CSSProperties = {};
+		style.width = `var(--element-${width})`;
+		return style;
+	};
+	const loaderWidth = getWidth();
+
+	return (
+		<div
+			className={`${Styles.loader} ${className}`}
+			style={{
+				...style,
+				...loaderWidth,
+			}}
+			{...props}
+		></div>
+	);
+};
