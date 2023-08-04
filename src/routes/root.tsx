951c99daa149cf4fd1382fd3fbe4d9e8b3cb56d0
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-08-04 22:14:28 +0300

    master: add vercel analytics

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { App } from '../components/App/App.tsx';
 
 export const Root = () => {
-	return <App />;
+	return (
+		<>
+			<App />
+			<Analytics />
+		</>
+	);
 };
