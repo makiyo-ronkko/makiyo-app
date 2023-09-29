2067e14e1a32089a6648bb569c71a13e26d7dacf
makiyo <makiyo.ronkko@haltian.com>
2023-09-29 15:37:16 +0300

    develop: feat: create a new Nav component

---
+++ src/components/Projects/ECLayout/Nav.tsx
@@ -0,0 +1,55 @@
+import { Button, Grid, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+
+const subNavigation = [
+	{
+		title: 'About us',
+		subTitle: ['Navigate 1', 'Navigate 2', 'Navigate 3'],
+	},
+	{
+		title: 'Contact us',
+		subTitle: ['Navigate 1', 'Navigate 2', 'Navigate 3', 'Navigate 4'],
+	},
+	{
+		title: 'Flequently asked questions',
+		subTitle: ['Navigate 1', 'Navigate 2', 'Navigate 3'],
+	},
+];
+
+export const Nav = () => {
+	return (
+		<nav>
+			<Toolbar sx={{ height: '400px' }}>
+				{subNavigation.map((category) => (
+					<Grid gap={2} container>
+						<Grid
+							container
+							height={50}
+							sx={{ justifyContent: 'center', alignItems: 'center' }}
+						>
+							<Typography
+								variant='button'
+								sx={{ textAlign: 'center', color: 'var(--color-dark)' }}
+							>
+								{category.title}
+							</Typography>
+						</Grid>
+						<Grid key={category.title} height={150} container>
+							{category.subTitle.map((t) => (
+								<Button
+									variant='text'
+									sx={{
+										color: 'var(--color-mud)',
+										height: 'var(--element-xs)',
+									}}
+								>
+									{t}
+								</Button>
+							))}
+						</Grid>
+					</Grid>
+				))}
+			</Toolbar>
+		</nav>
+	);
+};
