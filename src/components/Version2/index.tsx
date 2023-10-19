8b0a7eaab1d273f6bdf720aca91ced1f51c5ab29
makiyo <makiyo.ronkko@haltian.com>
2023-10-19 15:52:47 +0300

    develop: feat: render PixelArtApp

--- src/components/Version2/index.tsx
+++ src/components/Version2/index.tsx
@@ -1,12 +1,43 @@
 import React from 'react';
-import { RandomImage } from './RandomImage';
 import Styles from './Desgin.module.css';
+import { Typography } from '@mui/material';
+import PixelArtApp from './PixelArt';
 
 export const Design = () => {
 	return (
 		<div className={Styles.wrapper}>
-			<p className={Styles.h2}>Coming soon...</p>
-			<RandomImage />
+			<div className={Styles.textWrapper}>
+				<Typography
+					variant='h2'
+					sx={{
+						textTransform: 'uppercase',
+						color: 'var(--color-mud)',
+						fontWeight: 'bold',
+						textAlign: 'center',
+					}}
+				>
+					Create a pixel art
+				</Typography>
+				<div>
+					<Typography
+						variant='h6'
+						sx={{
+							color: 'var(--color-mud)',
+						}}
+					>
+						1. Pick a color
+					</Typography>
+					<Typography
+						variant='h6'
+						sx={{
+							color: 'var(--color-mud)',
+						}}
+					>
+						2. Click or drag on the canvas
+					</Typography>
+				</div>
+			</div>
+			<PixelArtApp />
 		</div>
 	);
 };
