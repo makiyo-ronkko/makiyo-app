459ac81e10a0e0eaff9ebd58d8e96e492e760579
makiyo <makiyo.ronkko@haltian.com>
2023-10-20 09:57:52 +0300

    develop: feat: highlight selected color

--- src/components/Version2/ColorPalette/ColorPalette.tsx
+++ src/components/Version2/ColorPalette/ColorPalette.tsx
@@ -1,33 +1,45 @@
 import React from 'react';
 import Styles from './ColorPalette.module.css';
+import { classNames } from '../../../utils/classNames';
 
 interface ColorPaletteProp {
 	onSelectColor: (val: string) => void;
+	selectedColor: string | undefined;
 }
 
-export const ColorPalette = ({ onSelectColor }: ColorPaletteProp) => {
-	const colors = [
-		'var(--color-default)',
-		'var(--color-mud)',
-		'#000000',
-		'var(--color-nude)',
-		'var(--color-tomato)',
-		'var(--color-sunflower)',
-		'var(--color-sunflower-dark)',
-		'var(--color-forest)',
-		'var(--color-forest-dark)',
-		'var(--color-primary)',
-		'var(--color-primary-dark)',
-		'var(--color-secondary)',
-		'var(--color-secondary-dark)',
-	];
+const colors = [
+	{ id: 1, color: 'var(--color-default)' },
+	{ id: 2, color: 'var(--color-mud)' },
+	{ id: 3, color: '#000000' },
+	{ id: 4, color: 'var(--color-nude)' },
+	{ id: 5, color: 'var(--color-tomato)' },
+	{ id: 6, color: 'var(--color-sunflower)' },
+	{ id: 7, color: 'var(--color-sunflower-dark)' },
+	{ id: 8, color: 'var(--color-forest)' },
+	{ id: 9, color: 'var(--color-forest-dark)' },
+	{ id: 10, color: 'var(--color-primary)' },
+	{ id: 11, color: 'var(--color-primary-dark)' },
+	{ id: 12, color: 'var(--color-secondary)' },
+	{ id: 13, color: 'var(--color-secondary-dark)' },
+];
+
+export const ColorPalette = ({
+	onSelectColor,
+	selectedColor,
+}: ColorPaletteProp) => {
+	const selectedColorFromPalette = (color: string) => {
+		if (!selectedColor && !color) return;
+		return selectedColor === color;
+	};
 
 	return (
 		<div className={Styles.colorPalette}>
-			{colors.map((color, index) => (
+			{colors.map(({ color, id }) => (
 				<button
-					key={index}
-					className={Styles.colorButton}
+					key={id}
+					className={`${Styles.colorButton} ${classNames({
+						[Styles.selectedColor]: selectedColorFromPalette(color),
+					})}`}
 					style={{ backgroundColor: color }}
 					onClick={() => onSelectColor(color)}
 				></button>
