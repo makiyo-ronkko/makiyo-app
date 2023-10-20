638457d86ad1665f9b3356a73e423a15d275e972
makiyo <makiyo.ronkko@haltian.com>
2023-10-20 11:00:07 +0300

    develop: feat: add ComfirmationModal

--- src/components/Version2/PixelArt.tsx
+++ src/components/Version2/PixelArt.tsx
@@ -6,11 +6,29 @@ import Styles from './PixelArt.module.css';
 import { Button } from '@mui/material';
 import { PixelArtCanvas } from './PixelArtCanvas/PixelArtCanvas';
 import { ColorPalette } from './ColorPalette/ColorPalette';
+import ConfirmationModal from './ConfirmationModal/ConfirmationModal';
 
 const ID = 'pixel-canvas';
 
 const PixelArtApp = () => {
 	const [selectedColor, setSelectedColor] = useState<string>();
+	const [confirmationModalOpen, setConfirmationModalOpen] =
+		useState<boolean>(false);
+
+	const handleModal = () => {
+		setConfirmationModalOpen(true);
+	};
+
+	const handleModalClose = () => {
+		setConfirmationModalOpen(false);
+	};
+
+	const handleSaveFile = () => {
+		if (confirmationModalOpen) {
+			saveAsPNG();
+		}
+		handleModalClose();
+	};
 
 	const saveAsPNG = async () => {
 		const canvas = document.getElementById(ID);
@@ -29,9 +47,24 @@ const PixelArtApp = () => {
 				selectedColor={selectedColor}
 			/>
 			<PixelArtCanvas id={ID} selectedColor={selectedColor} />
-			<Button variant='outlined' className={Styles.button} onClick={saveAsPNG}>
+			<Button
+				variant='contained'
+				className={Styles.button}
+				onClick={handleModal}
+				sx={{
+					color: 'var(--color-default)',
+					backgroundColor: 'var(--color-primary-dark)',
+				}}
+			>
 				Save as PNG
 			</Button>
+			{confirmationModalOpen && (
+				<ConfirmationModal
+					open={confirmationModalOpen}
+					handleClose={handleModalClose}
+					handleConfirm={handleSaveFile}
+				/>
+			)}
 		</div>
 	);
 };
