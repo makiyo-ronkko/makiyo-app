ead9887b1c66a485746c38c3f67f2d52b23dac25
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-11-24 16:11:34 +0200

    develop: refactor: move ThemeProvider to the root

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,15 +2,12 @@ import React from 'react';
 import AppStyles from './App.module.css';
 import { MainStyle } from '../Main/index.tsx';
 import { ScrollToTop } from '../../helpers/scroll.tsx';
-import { ThemeProvider } from '../hooks/ThemeContext';
 
 export const App = () => {
 	return (
-		<ThemeProvider>
-			<div className={AppStyles.mainWrapper}>
-				<ScrollToTop />
-				<MainStyle />
-			</div>
-		</ThemeProvider>
+		<div className={AppStyles.mainWrapper}>
+			<ScrollToTop />
+			<MainStyle />
+		</div>
 	);
 };
