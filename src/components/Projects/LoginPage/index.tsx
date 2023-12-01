713e7b03a60fbd185a27f60bd62c53deed1ff49e
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-12-01 23:58:56 +0200

    develop: feat: add dark theme

--- src/components/Projects/LoginPage/index.tsx
+++ src/components/Projects/LoginPage/index.tsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 import Styles from './LoginPage.module.css';
 import { AppProvider } from './AppContext';
 import { LandingPage } from './MainPage';
+import { useTheme } from '../../hooks/ThemeContext';
 
 export const LoginPage = () => {
+	const { isDarkMode } = useTheme();
+
 	return (
 		<AppProvider>
-			<div className={Styles.wrapper}>
+			<div
+				className={`${Styles.wrapper} ${
+					isDarkMode ? Styles.darkModeBackground : Styles.defaultModeBackground
+				}`}
+			>
 				<div className={Styles.button}>
 					<Link to={'/'} className={Styles.backButton}>
 						Back
