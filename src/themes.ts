9d64e6694d182d8229dbc5c01b7ed137bb043038
makiyo-ronkko <makiyo.ronkko@gmail.com>
2023-12-07 14:28:25 +0200

    develop: chore: delete palette

--- src/themes.ts
+++ src/themes.ts
@@ -1,11 +1,6 @@
 import { createTheme } from '@mui/material/styles';
 
 export const appTheme = createTheme({
-	palette: {
-		primary: { main: '#b3d6ff' },
-		secondary: { main: '#f78094' },
-		mode: 'dark',
-	},
 	typography: {
 		fontFamily: ['Source Sans Pro', 'sans-serif'].join(','),
 	},
