ac7b3cb128d42feed2559cdaaf8432b51901ab56
makiyo-ronkko <makiyo.ronkko@gmail.com>
2024-02-02 14:29:55 +0200

    develop: perf: improve peformance on image loading

---
+++ src/components/Projects/MultiColumnLayout/images.ts
@@ -0,0 +1,24 @@
+const ROOT = 'https://source.unsplash.com/random?';
+
+export const imageUrls = {
+  animal: `${ROOT}animal`,
+  flower: `${ROOT}flower`,
+  sky: `${ROOT}sky`,
+  car: `${ROOT}car`,
+  smile: `${ROOT}smile`,
+  tourism: `${ROOT}tourism`,
+  nature: `${ROOT}nature`,
+  europe: `${ROOT}europe`,
+  japan: `${ROOT}japan`,
+  shopping: `${ROOT}shopping`,
+  fashion: `${ROOT}fashion`,
+  mountain: `${ROOT}mountain`,
+  business: `${ROOT}business`,
+  hobby: `${ROOT}hobby`,
+  technology: `${ROOT}technology`,
+  game: `${ROOT}game`,
+  family: `${ROOT}family`,
+  city: `${ROOT}city`,
+  food: `${ROOT}food`,
+  beauty: `${ROOT}beauty`,
+};
--- src/components/Projects/MultiColumnLayout/index.tsx
+++ src/components/Projects/MultiColumnLayout/index.tsx
@@ -2,139 +2,141 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Divider, Grid, Typography } from '@mui/material';
 
-import styles from './MultiColumnLayout.module.css';
 import { ScrollToTop } from '../../../helpers/scroll';
 import { Header } from './Header';
 import { GridCard } from './Card';
 import { generateRandomText } from '../../../helpers/textGenerator';
 import { useTheme } from '../../hooks/ThemeContext';
+import { imageUrls } from './images';
+
+import styles from './MultiColumnLayout.module.css';
 
 const cards = [
   {
     id: 1,
     title: 'Card 1',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?animal',
+    image: imageUrls.animal,
   },
   {
     id: 2,
     title: 'Card 2',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?flower',
+    image: imageUrls.flower,
   },
   {
     id: 3,
     title: 'Card 3',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?sky',
+    image: imageUrls.sky,
   },
   {
     id: 4,
     title: 'Card 4',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?car',
+    image: imageUrls.car,
   },
   {
     id: 5,
     title: 'Card 5',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?smile',
+    image: imageUrls.smile,
   },
   {
     id: 6,
     title: 'Card 6',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?tourism',
+    image: imageUrls.tourism,
   },
   {
     id: 7,
     title: 'Card 7',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?nature',
+    image: imageUrls.nature,
   },
   {
     id: 8,
     title: 'Card 8',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?europe',
+    image: imageUrls.europe,
   },
   {
     id: 9,
     title: 'Card 9',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?japan',
+    image: imageUrls.japan,
   },
   {
     id: 10,
     title: 'Card 10',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?shpping',
+    image: imageUrls.shopping,
   },
   {
     id: 11,
     title: 'Card 11',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?fashion',
+    image: imageUrls.shopping,
   },
   {
     id: 12,
     title: 'Card 12',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?mountain',
+    image: imageUrls.fashion,
   },
   {
     id: 13,
     title: 'Card 13',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?business',
+    image: imageUrls.mountain,
   },
   {
     id: 14,
     title: 'Card 14',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?hobby',
+    image: imageUrls.business,
   },
   {
     id: 15,
     title: 'Card 15',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?technology',
+    image: imageUrls.hobby,
   },
   {
     id: 16,
     title: 'Card 16',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?cooking',
+    image: imageUrls.technology,
   },
   {
     id: 17,
     title: 'Card 17',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?game',
+    image: imageUrls.game,
   },
   {
     id: 18,
     title: 'Card 18',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?family',
+    image: imageUrls.family,
   },
   {
     id: 19,
     title: 'Card 19',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?city',
+    image: imageUrls.city,
   },
   {
     id: 20,
     title: 'Card 20',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?food',
+    image: imageUrls.food,
   },
   {
     id: 21,
     title: 'Card 21',
     content: generateRandomText(2),
-    image: 'https://source.unsplash.com/random?beauty',
+    image: imageUrls.beauty,
   },
 ];
 
